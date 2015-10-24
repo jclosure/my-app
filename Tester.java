e72ccfe40abe1d0149dcca89243234b3956a83be
Joel Holder <jclosure@gmail.com>
2015-10-23 23:02:23 -0500

    cleanup and documentation

--- Tester.java
+++
@@ -1,6 +0,0 @@
-public class Tester {
-  public static void main(String[] args) {
-    App.main(new String[] { "One", "Two", "Three" });
-
-  }
-}
--- com.mycompany.app.App.java
+++
@@ -1,6 +0,0 @@
-
-import com.mycompany.app.App
-
-public static void main(String[] args) {
-  App.main({ "One", "Two", "Three" });
-}
